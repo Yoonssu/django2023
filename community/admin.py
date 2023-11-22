62e883efff029959ee2260c324f5892a04c98464
sieun1002 <nata20034@naver.com>
2023-11-23 07:38:29 +0900

    user 추가 및 models.py에 tables 추가

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Post, Comment, Team, Major, Keyword
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Team)
+admin.site.register(Major)
+admin.site.register(Keyword)
+
--- community/models.py
+++ community/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    post = models.ManyToManyField('Post', related_name='users')
+    major = models.ManyToManyField('Major', related_name='users')
+    keyword = models.ManyToManyField('Keyword', related_name='users')
+    password = models.CharField(max_length=255)
+    email = models.EmailField()
+
+class Post(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    is_major = models.BooleanField()
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    img = models.TextField(null=True)
+    time = models.DateTimeField()
+    duksung = models.BooleanField()
+
+class Comment(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    team = models.ForeignKey('Team', on_delete=models.CASCADE)
+    content = models.TextField()
+    time = models.DateTimeField()
+    secret = models.BooleanField()
+
+class Team(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    time = models.DateTimeField()
+
+class Major(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    major = models.CharField(max_length=255)
+
+class Keyword(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    label = models.CharField(max_length=255)
+    is_make = models.BooleanField()
+    category = models.CharField(max_length=255, null=True)
