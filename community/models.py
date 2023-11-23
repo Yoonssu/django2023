19f0283ee4eaa13fbce761ec9dc159b193b53e33
sieun1002 <nata20034@naver.com>
2023-11-23 22:56:48 +0900

    models.py 완성 (변수 이름 변경 및 전체적인 수정)

--- community/models.py
+++ community/models.py
@@ -3,36 +3,44 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     id = models.BigAutoField(primary_key=True)
-    # userId = models.CharField(max_length=40)
     major = models.ManyToManyField('Major', related_name='users')
     keyword = models.ManyToManyField('Keyword', related_name='users')
+    username = models.CharField(max_length=225)
     password = models.CharField(max_length=255)
     email = models.EmailField()
     def __str__(self):
-        return f'{self.id}'
+        return f'{self.username}'
 
+class Keyword(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    keywordname = models.CharField(max_length=255)
+    ismake = models.BooleanField()
+    category = models.CharField(max_length=255, null=True)
+    def __str__(self):
+        return f'{self.keywordname}'
 
+class Major(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    majorname = models.CharField(max_length=255)
+    def __str__(self):
+        return f'{self.majorname}'
 
 class Post(models.Model):
     id = models.BigAutoField(primary_key=True)
-    is_major = models.BooleanField()
+    major = models.ForeignKey(Major, on_delete=models.SET_NULL, null=True, blank=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     img = models.TextField(null=True)
-    time = models.DateTimeField()
-    duksung = models.BooleanField()
+    time = models.DateTimeField(auto_now_add = True)
+    isduksung = models.BooleanField()
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
 
 class Scrap(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-class Comment(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
-    team = models.ForeignKey('Team', on_delete=models.CASCADE)
-    content = models.TextField()
-    time = models.DateTimeField()
-    secret = models.BooleanField()
+    def __str__(self):
+        return f'Scrap [{self.pk}] - {self.post.title} by {self.user.username}'
 
 class Team(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -40,18 +48,17 @@ class Team(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    time = models.DateTimeField()
-
-class Major(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    major = models.CharField(max_length=255)
+    time = models.DateTimeField(auto_now_add = True)
     def __str__(self):
-        return f'{self.major}'
+        return f'[{self.pk}]{self.title} :: {self.user}'
 
-class Keyword(models.Model):
+class Comment(models.Model):
     id = models.BigAutoField(primary_key=True)
-    label = models.CharField(max_length=255)
-    is_make = models.BooleanField()
-    category = models.CharField(max_length=255, null=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    team = models.ForeignKey('Team', on_delete=models.CASCADE)
+    content = models.TextField()
+    time = models.DateTimeField(auto_now_add = True)
+    issecret = models.BooleanField()
     def __str__(self):
-        return f'{self.label}'
+        return f'Comment [{self.pk}] on "{self.team.post.title}" by {self.user.username}'
+
