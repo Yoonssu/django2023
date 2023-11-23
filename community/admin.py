9278a093fca1f2b500bb95b935bd81b5e88f3011
sieun1002 <nata20034@naver.com>
2023-11-23 21:09:16 +0900

    modles.py에 scrap class 추가 및 admin.py에 생성한 class import

--- community/admin.py
+++ community/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Post, Comment, Team, Major, Keyword
+from .models import User, Post, Comment, Team, Major, Keyword, Scrap
 
 # Register your models here.
 admin.site.register(User)
@@ -8,4 +8,5 @@ admin.site.register(Comment)
 admin.site.register(Team)
 admin.site.register(Major)
 admin.site.register(Keyword)
+admin.site.register(Scrap)
 
--- community/models.py
+++ community/models.py
@@ -3,11 +3,15 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     id = models.BigAutoField(primary_key=True)
-    post = models.ManyToManyField('Post', related_name='users')
+    # userId = models.CharField(max_length=40)
     major = models.ManyToManyField('Major', related_name='users')
     keyword = models.ManyToManyField('Keyword', related_name='users')
     password = models.CharField(max_length=255)
     email = models.EmailField()
+    def __str__(self):
+        return f'{self.id}'
+
+
 
 class Post(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -17,6 +21,10 @@ class Post(models.Model):
     img = models.TextField(null=True)
     time = models.DateTimeField()
     duksung = models.BooleanField()
+
+class Scrap(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
 class Comment(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -37,9 +45,13 @@ class Team(models.Model):
 class Major(models.Model):
     id = models.BigAutoField(primary_key=True)
     major = models.CharField(max_length=255)
+    def __str__(self):
+        return f'{self.major}'
 
 class Keyword(models.Model):
     id = models.BigAutoField(primary_key=True)
     label = models.CharField(max_length=255)
     is_make = models.BooleanField()
     category = models.CharField(max_length=255, null=True)
+    def __str__(self):
+        return f'{self.label}'
