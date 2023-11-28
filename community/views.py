13589cd0088fc90d0fc594ea684ec63f23016150
sieun1002 <nata20034@naver.com>
2023-11-28 21:32:23 +0900

    [modify] recommend.html nav 오류 나던 거 해결 (페이지 제대로 로딩됨)

--- community/views.py
+++ community/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Post, User, Team 
+from .models import Post, User, Team, Major, Keyword
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
+from django.db.models import Q 
 
 # Create your views here.
 class PostList(ListView):
@@ -59,12 +60,27 @@ class TeamDetail(DetailView):
         context = super(TeamDetail, self).get_context_data()
         return context
 
+
 def recommend(request, pk):
-    posts = Post.objects.all()
-    user = User.objects.all()
+    user = get_object_or_404(User, pk=pk)
+
+    # 사용자가 선택한 키워드에 맞는 게시글 3개 가져오기(제가 임의로 최신 3개라는 추천 로직을 쓴 거고, 실제 로직을 이 틀에서 변형해서 구현하셔야 해요 )
+    selected_keywords = user.keyword.all()
+    recommended_posts = Post.objects.filter(
+        Q(title__icontains=selected_keywords) | Q(content__icontains=selected_keywords)
+    ).order_by('-time')[:3]
+
+    # 사용자가 선택한 전공에 맞는 게시물들 가져오기
+    selected_majors = user.major.all()
+    major_posts = {}
+    for major in selected_majors:
+        posts = Post.objects.filter(major=major)
+        major_posts[major] = posts
 
     context = {
-        'posts': posts,
-        'user': user
+        'user': user,
+        'recommended_posts': recommended_posts,
+        'major_posts': major_posts,
     }
+
     return render(request, 'community/recommend.html', context)
