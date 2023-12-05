f674ab83208c01413ecad007ccc7d0ce65f8e2fc
kitechoi <chevel0212@gmail.com>
2023-12-06 01:55:41 +0900

    [FEAT]예서+민지님 post title, post_team_title 중복되지 않게 분할 적용함 + 폰트 크기 조정

--- community/forms.py
+++ community/forms.py
@@ -17,50 +17,57 @@ class TeamPostForm(forms.ModelForm):
         queryset=Post.objects.all(),
         empty_label=None,
         to_field_name='id',
-
     )
 
     class Meta:
         model = Team
-        fields = ['post', 'title', 'content' ]
+        fields = ['post', 'title', 'content']
 
-    def __init__(self, user, *args, **kwargs):
+    def __init__(self, user, post_id, *args, **kwargs):
         super(TeamPostForm, self).__init__(*args, **kwargs)
         self.user = user
 
+        # post_id를 이용하여 Post 객체 가져오기 (기본값 사용)
+        post_instance = get_object_or_404(Post, id=post_id) if post_id else None
+
+        # 폼의 post 필드 초기값 설정
+        self.fields['post'].initial = post_instance
+
         self.fields['title'].label = '제목'
-        self.fields['post'].label = '활동명'
         self.fields['content'].label = '내용'
+        self.fields['post'].label = '활동명'
 
     def save(self, commit=True):
         instance = super().save(commit=False)
         current_user = self.user
-        if current_user.is_authenticated:
-            post_title_instance = self.cleaned_data['post']
 
-            # 괄호 안의 내용을 제거하는 방식
-            import re
-            cleaned_post_title = re.sub(r'\[\d+\]', '', str(post_title_instance)).strip()
+        if current_user.is_authenticated:
+            post_instance_pk = self.cleaned_data['post'].id  # post 필드에 해당하는 Post 객체의 primary key
 
             try:
-                post_instance = Post.objects.get(title=cleaned_post_title)
+                post_instance = Post.objects.get(pk=post_instance_pk)
                 instance.post = post_instance
 
-                # 합쳐진 title을 생성하여 저장
+                # 괄호 안의 내용을 제거하는 방식
+                import re
+                cleaned_post_title = re.sub(r'\[\d+\]', '', str(post_instance)).strip()
+
                 user_input_title = self.cleaned_data['title']
                 combined_title = f"[{cleaned_post_title}] {user_input_title}"
-                instance.title = combined_title
+                instance.title = user_input_title
 
                 instance.user = current_user
                 if commit:
                     instance.save()
                 return instance
+
             except Post.DoesNotExist:
                 # Post 객체를 찾을 수 없는 경우
                 return None
         else:
             # 인증되지 않은 사용자에 대한 처리 (예: 리디렉션)
             return redirect('/community')
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
